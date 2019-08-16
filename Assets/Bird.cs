5d8101f12626420a79a080c0dd141472540baedb
nunu-e64 <fumiya.amano@gree.co.jp>
2019-08-16 10:50:21 +0900

    complete tutorial

---
+++ Assets/Bird.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public GameObject panelGameOver;
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        panelGameOver.SetActive(true);
+    }
+    public void Fly()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 1f);
+    }
+}
